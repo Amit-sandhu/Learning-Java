64a48e3bbdcf2844d5bd7249f6159c3534867b23
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-07-03 13:03:30 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class arrays {
-    // same as C
+    // same as C more or less
     public static void main(String[] args){
         String[] fruits = {"banana","apple","pineapple","orange"};
         System.out.println(fruits); // return default string representation of the array object
