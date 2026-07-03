530be02aeebb73878afb811e250261b9629e49ff
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-07-03 13:02:25 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class arrays {
+    // same as C
+    public static void main(String[] args){
+        String[] fruits = {"banana","apple","pineapple","orange"};
+        System.out.println(fruits); // return default string representation of the array object
+        System.out.println(Arrays.toString(fruits)); // to print an array
+        System.out.println(fruits.length);
+        System.out.println(fruits[1]);
+        Arrays.sort(fruits);
+        System.out.println(Arrays.toString(fruits));
+    }
+}
