b6575970d790ad68f60cfe77259ff73c329e2864
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-07-08 10:11:09 +0530

    added new functions to array.java

--- arrays.java
+++ arrays.java
@@ -3,12 +3,51 @@ import java.util.Arrays;
 public class arrays {
     // same as C more or less
     public static void main(String[] args){
+
+
         String[] fruits = {"banana","apple","pineapple","orange"};
-        System.out.println(fruits); // return default string representation of the array object
-        System.out.println(Arrays.toString(fruits)); // to print an array
-        System.out.println(fruits.length);
-        System.out.println(fruits[1]);
-        Arrays.sort(fruits);
-        System.out.println(Arrays.toString(fruits));
+
+        System.out.println(fruits);                                                 // return default string representation of the array object
+        // System.out.println(Arrays.toString(fruits));                                // to print an array
+
+        // System.out.println(fruits.length);
+        // System.out.println(fruits[1]);
+
+        // Arrays.sort(fruits);
+        // System.out.println(Arrays.toString(fruits));
+        
+        
+        // student();
+        // char_array();
+        // bool_array();
+        
+        
+    }
+    
+    void student(){
+        int[] students;           //null
+        students = new int[5];
+        
+        // int[] students = new int[5];   same as above
+        
+        System.out.println(students);
+        System.out.println(Arrays.toString(students));
+        
+        System.out.println(students.getClass().getName());
+        System.out.println(students.hashCode());
+        System.out.println(students.getClass().getName() + "@" + Integer.toHexString(hashCode()));
+        
+    }
+    
+    static void char_array(){
+        char[] ch = new char[4];
+        System.out.println(ch);
+        // System.out.println(Arrays.toString(ch));
+    }
+
+    static void bool_array(){
+        boolean[] bool = new boolean[4];
+        System.out.println(bool);
+        // System.out.println(Arrays.toString(bool));
     }
 }
