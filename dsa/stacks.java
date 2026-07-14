aeba1a3ab593a8f22b6f93bca365c16cf6e03fce
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-07-14 09:22:17 +0530

    added stacks.java file

--- dsa/stacks.java
+++ dsa/stacks.java
@@ -1,4 +1,25 @@
-
+import java.util.Stack;
 public class stacks {
+     public static void main(String[] args){
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Minecraft");
+        stack.push("GTAV");
+        stack.push("Spiderman2");
+        stack.push("RDR");
+
+        System.out.println(stack);
         
+        stack.pop();
+        System.out.println(stack);
+
+        System.out.println(stack.isEmpty());
+
+        stack.pop();
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+     }   
 }
